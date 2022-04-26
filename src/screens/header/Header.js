8192fae4d0df9c8908bad0f3b1d5eee71c98728f
Adam G <adagun@hotmail.com>
2022-04-26 10:41:19 +0200

    fixed typed text box

--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -6,15 +6,14 @@ function Header() {
     <div className ="main-text">
         <h1>Hello</h1>
         <Typed
-                strings={[
-                    'Search for products',
-                    'Search for categories',
-                    'Search for brands']}
-                    typeSpeed={40}
-                    backSpeed={50}                  
-                    loop 
-                    
-                />
+            strings={[
+                'Search for products',
+                'Search for categories',
+                'Search for brands']}
+                typeSpeed={40}
+                backSpeed={50}                  
+                loop 
+    />
     </div>
   )
 }
