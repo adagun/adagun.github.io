9230f2eea2a757efe4fb505b7ad9439aca16ad92
Adam G <adagun@hotmail.com>
2022-04-27 12:38:46 +0200

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-
-import './App.css';
-import Navigation from './screens/navigationBar/NavigationBar'
-import  ParticlesBackground  from './components/ParticlesBackground';
-import Header from './screens/header/Header'
-import Github from './screens/github/Github'
-import Projects from './screens/projects/Projects';
+import "./App.css";
+import Navigation from "./screens/navigationBar/NavigationBar";
+import ParticlesBackground from "./components/ParticlesBackground";
+import Header from "./screens/header/Header";
+import Github from "./screens/github/Github";
+import Projects from "./screens/projects/Projects";
 
 function App() {
   return (
     <div className="App">
-     
-      <Navigation/>
-     
-  
-    <div >
-    <Header>
-      
-    </Header>
-      <Github/>
-      <Projects/>
-    </div>
-    <ParticlesBackground/>
+      <Navigation />
+
+      <div>
+        <Header></Header>
+        <Github />
+        <Projects />
+      </div>
+      <ParticlesBackground />
     </div>
   );
 }
 
 export default App;
 
-
-
 // todo add more css variables
-     
